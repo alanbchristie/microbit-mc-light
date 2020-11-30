36d1832bb2584bfc76b18f5d6699b08719378742
Alan B. Christie <alan@kaitak.co.uk>
2020-11-30 08:54:33 +0000

    Documentation and 'on logo' to display level

--- main.ts
+++ main.ts
@@ -1,3 +1,11 @@
+/**
+ * Simple code that lights the matrix in a spiral for a few seconds before clearing the display and then relying on the "A" and "B" buttons to decrease and increase the brightness of the central LED.
+ * 
+ * When the LED is fully extinguished (the default state) the left-hand LED of the central row is illuminated. When the central LED is at its brightest the right-hand LED of the central row is illuminated.
+ * 
+ * At any time the user can see the current level value by touching the MicroBit logo.
+ */
+// If "level" variable is not at its minimum value then decrement it (limiting it to the minimum) and call "light(level)" setting the central LED
 input.onButtonPressed(Button.A, function () {
     if (level > levelmin) {
         level += 0 - levelstep
@@ -7,6 +15,7 @@ input.onButtonPressed(Button.A, function () {
         light2(level)
     }
 })
+// Sets all the LEDs in the matrix in a clockwise spiral (starting top-left) with each LED increasing in brightness by steps of 10 from 10 to 250
 function sequence () {
     led.plotBrightness(0, 0, 10)
     led.plotBrightness(1, 0, 20)
@@ -34,6 +43,7 @@ function sequence () {
     led.plotBrightness(1, 2, 240)
     led.plotBrightness(2, 2, 250)
 }
+// If "level" variable is not at its maximum value then increase it (limiting it to the maximum) and call "light(level)" setting the central LED
 input.onButtonPressed(Button.B, function () {
     if (level < levelmax) {
         level += levelstep
@@ -43,6 +53,7 @@ input.onButtonPressed(Button.B, function () {
         light2(level)
     }
 })
+// A function, provided with a "level" value -  expected to be the light level of an LED, that sets the brightness of the central LED to that level and then also illuminates the left-hand LED of the central row if the level is at or below the minimum or the right-hand LED if at or above the maximum.
 function light2 (num: number) {
     led.plotBrightness(2, 2, num)
     if (num >= levelmax) {
@@ -56,15 +67,30 @@ function light2 (num: number) {
         led.unplot(0, 2)
     }
 }
+// If initialised this displays the current level in the LED matrix before returning the matrix to its normal state.
+input.onLogoEvent(TouchButtonEvent.Released, function () {
+    if (initialised) {
+        basic.clearScreen()
+        basic.showNumber(level)
+        basic.pause(1000)
+        light2(level)
+    }
+})
+// Initialises the app by setting the initial LED level, its upper and lower limits and a "step" (increment) rate.
+// 
+// It then calls "sequence()" before waiting a few seconds, clearing the screen and initialising the app main display.
+let level = 0
 let levelstep = 0
 let levelmax = 0
 let levelmin = 0
-let level = 0
-level = 0
+let initialised = 0
+initialised = 0
 levelmin = 0
 levelmax = 255
 levelstep = 8
+level = levelstep * 4
 sequence()
-basic.pause(2000)
+basic.pause(5000)
 basic.clearScreen()
 light2(level)
+initialised = 1
